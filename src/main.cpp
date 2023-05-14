b318abe14ffbe0467b1291ad64a55cc50d84ab27
Hà Vĩnh Kiện <20520597@gm.uit.edu.vn>
2023-05-14 23:37:16 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
+#include <WiFiClient.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "Adafruit_AHTX0.h"
@@ -11,7 +11,7 @@ const char* password = "28112001";
 
 // Thiết lập thông tin broker MQTT
 const char* mqtt_server = "641712bdeca84cd99a23e6b291cbf298.s2.eu.hivemq.cloud";
-const uint16_t mqtt_port = 8883;
+const uint16_t mqtt_port = 1883;
 const char* mqtt_user = "20520597";
 const char* mqtt_password = "Brs13125";
 
@@ -19,70 +19,70 @@ const char* mqtt_password = "Brs13125";
 const char* pub_topic = "20520597/pub";
 const char* sub_topic = "esp32/sub";
 
-// Khai báo đối tượng MQTT client và đối tượng WiFiClientSecure
-WiFiClientSecure wifiClient;
-PubSubClient client(wifiClient);
+// Khai báo đối tượng MQTT client và đối tượng WiFiClient
+WiFiClient wifiClient;
+PubSubClient client(mqtt_server, mqtt_port, wifiClient);
 
 // Khai báo đối tượng cảm biến nhiệt độ AHT20
 Adafruit_AHTX0 aht;
 
 // Hàm kết nối đến mạng WiFi
 void connectToWiFi() {
-Serial.print("Đang kết nối WiFi...");
-WiFi.begin(ssid, password);
+  Serial.print("Đang kết nối WiFi...");
+  WiFi.begin(ssid, password);
 
-while (WiFi.status() != WL_CONNECTED) {
-delay(500);
-Serial.print(".");
-}
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-Serial.println(" Đã kết nối với WiFi");
+  Serial.println(" Đã kết nối với WiFi");
 }
 
 // Hàm kết nối đến MQTT broker
 void connectToMQTT() {
-Serial.print("Đang kết nối MQTT broker...");
-String clientId = "ESP32-"; // Định danh cho đối tượng kết nối
-clientId += String(random(0xffff), HEX);
+  Serial.print("Đang kết nối MQTT broker...");
+  String clientId = "ESP32-"; // Định danh cho đối tượng kết nối
+  clientId += String(random(0xffff), HEX);
 
-wifiClient.setInsecure(); 
-client.setServer(mqtt_server, mqtt_port); 
+  client.setServer(mqtt_server, mqtt_port); 
 
-if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) { 
-Serial.println(" Đã kết nối với MQTT broker"); 
-}
-else { 
-Serial.print("Kết nối thất bại, mã lỗi: "); 
-Serial.print(client.state());
-Serial.println(", thử lại sau 5 giây");
-delay(5000);
+  if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) { 
+    Serial.println(" Đã kết nối với MQTT broker"); 
+  }
+  else { 
+    Serial.print("Kết nối thất bại, mã lỗi: "); 
+    Serial.print(client.state());
+    Serial.println(", thử lại sau 5 giây");
+    delay(5000);
+  }
 }
 
 void setup() {
-Serial.begin(9600); 
+  Serial.begin(9600); 
 
-connectToWiFi(); 
+  connectToWiFi(); 
 
-if (!aht.begin()) { 
-Serial.println("Không tìm thấy cảm biến AHT20!");
-while (1);
-}
+  if (!aht.begin()) { 
+    Serial.println("Không tìm thấy cảm biến AHT20!");
+    while (1);
+  }
 
-connectToMQTT();
+  connectToMQTT();
 }
 
 void loop() {
-sensors_event_t humidity, temp;
-aht.getEvent(NULL, &temp); // Đọc nhiệt độ từ cảm biến AHT20
+  sensors_event_t humidity, temp;
+  aht.getEvent(NULL, &temp); // Đọc nhiệt độ từ cảm biến AHT20
 
-StaticJsonDocument<200> doc; // Tạo đối tượng StaticJsonDocument để lưu trữ dữ liệu JSON
-doc["Nhiệt độ"] = temp.temperature; // Lưu nhiệt độ vào đối tượng StaticJsonDocument
-char jsonBuffer[512]; // Tạo bộ đệm để lưu trữ chuỗi JSON
-serializeJson(doc, jsonBuffer); // Chuyển đối tượng StaticJsonDocument sang chuỗi JSON và lưu vào bộ đệm
+  StaticJsonDocument<200> doc; // Tạo đối tượng StaticJsonDocument để lưu trữ dữ liệu JSON
+  doc["Nhiệt độ"] = temp.temperature; // Lưu nhiệt độ vào đối tượng StaticJsonDocument
+  char jsonBuffer[512]; // Tạo bộ đệm để lưu trữ chuỗi JSON
+  serializeJson(doc, jsonBuffer); // Chuyển đối tượng StaticJsonDocument sang chuỗi JSON và lưu vào bộ đệm
 
-client.publish(pub_topic, jsonBuffer); // Gửi chuỗi JSON đến MQTT broker
+  client.publish(pub_topic, jsonBuffer); // Gửi chuỗi JSON đến MQTT broker
 
-Serial.println(jsonBuffer); // Hiển thị chuỗi JSON trên Serial Monitor
+  Serial.println(jsonBuffer); // Hiển thị chuỗi JSON trên Serial Monitor
 
-delay(5000);
+  delay(5000);
 }
